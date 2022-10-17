3f52928bf4ff8996544b788dd19c446466e44e21
Don Tran <donqt15@gmail.com>
2022-10-16 23:40:22 -0700

    removed unnecessary files & attempted pom change

--- src/main/java/com/nighthawk/spring_portfolio/controllers/RosterTableController.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/RosterTableController.java
@@ -3,9 +3,9 @@ package com.nighthawk.spring_portfolio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-import com.nighthawk.spring_portfolio.mvc.students.RosterJpaRepository;
+import com.scioly_backend.student_roster.mvc.students.RosterJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.nighthawk.spring_portfolio.mvc.students.Roster;
+import com.scioly_backend.student_roster.mvc.students.Roster;
 import java.util.List;
 
 @Controller // HTTP requests are handled as a controller, using the @Controller annotation
